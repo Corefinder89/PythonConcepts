d7645ed45b22cad2681e27d5923f02fab3d44162
Corefinder89 <soumyajit.basu1988@outlook.com>
2021-05-01 18:25:17 +0530

    Add marker in pytest 	modified:   selenium-codes/execute.sh 	modified:   selenium-codes/pytest.ini 	modified:   selenium-codes/test/test_google.py

--- selenium-codes/execute.sh
+++ selenium-codes/execute.sh
@@ -2,10 +2,18 @@
 
 # Check if html_reports directory is present
 if [ -d "html_reports" ]; then
-    echo "html_reports already present in path"
+    echo "directory html_reports already present in path"
 else
     mkdir html_reports
     echo "directory html_reports created"
 fi
 
-pytest test -s -v --html=html_reports/reports.html --log-file logs/"$(date '+%F_%H:%M:%S')".log
+# Check if html_reports directory is present
+if [ -d "logs" ]; then
+    echo "directory logs already present in path"
+else
+    mkdir logs
+    echo "directory logs created"
+fi
+
+pytest test -s -v -m execute --html=html_reports/reports.html --log-file logs/"$(date '+%F_%H:%M:%S')".log
--- selenium-codes/test/test_google.py
+++ selenium-codes/test/test_google.py
@@ -1,7 +1,9 @@
 from utility.driverutility import Driverutility
+import pytest
 
 
 class Testgoogle(Driverutility):
+    @pytest.mark.execute
     def test_chrome(self):
         headless_status = super().parse_config("project_config", "browser_config", "headless_status")
         driver = super().spawn_driver("chrome", headless_status)
@@ -10,6 +12,7 @@ class Testgoogle(Driverutility):
 
         super().tear_down(driver)
 
+    @pytest.mark.execute
     def test_safari(self):
         headless_status = super().parse_config("project_config", "browser_config", "headless_status")
         driver = super().spawn_driver("safari", headless_status)
@@ -18,8 +21,9 @@ class Testgoogle(Driverutility):
 
         super().tear_down(driver)
 
+    @pytest.mark.debug
     def test_chrome_headless(self):
-        headless_status = True
+        headless_status = super().parse_config("project_config", "browser_config", "headless_status")
         driver = super().spawn_driver("chrome", headless_status)
         driver.get("https://www.google.co.in/")
         assert driver.title == "Google", "Title does not match"
